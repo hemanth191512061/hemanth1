2e771412f451e99aafd8a4a4efa638332fd47bf0
hemanth191512061 <40885806+hemanth191512061@users.noreply.github.com>
2018-07-15 18:21:09 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,13 @@
+N=int(input("enter the input"))
+if(N<=1000):
+ for k in range(2,N):
+    if N%k==0:
+        print("no")
+        break
+    else:
+        print("yes")
+        break
+else:
+    print("no")
+    
+        
