f80ce3c8c929c8d1c16136ac5eab4e08d881474e
hemanth191512061 <40885806+hemanth191512061@users.noreply.github.com>
2018-07-18 09:26:25 +0000

    Update and rename amstrong.py to amstrong6.py

--- amstrong6.py
+++ amstrong6.py
@@ -1,12 +1,12 @@
-x=int(input(""))
+z=int(input(""))
 sum=0
 rem=0
-k=x
+k=z
 while k>0:
     rem=k%10
     sum=sum*10+rem
     k=k//10
-if(sum==x):
+if(sum==z):
     print("yes")
 else:
     print("no")
